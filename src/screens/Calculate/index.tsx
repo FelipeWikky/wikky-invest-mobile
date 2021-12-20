5d224106f0addaacb181cd2a69938decc5ccb28f
Felipe Lima <felipewikky@gmail.com>
2021-12-20 13:53:22 -0300

    Created file to calculate screen

---
+++ src/screens/Calculate/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Calculate = () => {
+    return (
+        <View>
+            <Text>Calculate</Text>
+        </View>
+    );
+};
+
+export default Calculate;
