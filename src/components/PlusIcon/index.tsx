048ff332a71757ebe43546efd2b498c73d55f238
Felipe Lima <felipewikky@gmail.com>
2021-12-21 12:53:50 -0300

    Added plus icon component

---
+++ src/components/PlusIcon/index.tsx
@@ -0,0 +1,26 @@
+import { TouchableOpacity, Text } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+import styles from "./styles";
+
+interface Props {
+    onClick?: () => void;
+    size?: number;
+    iconColor?: string;
+}
+/**
+ * use this component to render a plus icon standardized with background green and icon color white
+ * @default size = 26, color = '#FFF'
+ */
+const PlusIcon = ({onClick, size, iconColor}: Props) => {
+    return (
+        <TouchableOpacity
+            style={styles.iconContainer}
+            onPress={() => onClick && onClick()}
+        >
+            <MaterialIcons name="add" size={size || 26} color={iconColor || "#FFF"} />
+        </TouchableOpacity>
+    )
+};
+
+export default PlusIcon;
---
+++ src/components/PlusIcon/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    iconContainer: {
+        backgroundColor: 'green',
+        borderRadius: 50,
+    }
+});
