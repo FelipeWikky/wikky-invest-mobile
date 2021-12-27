4312a3b261cdc0ad5a2b69aaf48fa3c6ee0b749d
Felipe Lima <felipewikky@gmail.com>
2021-12-26 22:26:43 -0300

    Added save and back features in wallet modal

--- src/components/WalletItemModal/index.tsx
+++ src/components/WalletItemModal/index.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { Modal, View, Text, TouchableOpacity, TextInput } from "react-native";
+import { Modal, View, Text, TouchableOpacity, TextInput, KeyboardAvoidingView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Wallet, getTypeFormatted } from "../../models/Wallet";
@@ -44,61 +44,63 @@ const WalletItemModal = (props: Props) => {
             transparent={true}
             animationType="slide"
         >
-            <SafeAreaView style={{ flex: 1 }}>
-                <View style={styles.modal}>
-                    <View style={styles.card}>
+            <KeyboardAvoidingView style={{ flex: 1 }}>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={styles.modal}>
+                        <View style={styles.card}>
 
-                        <View style={styles.header}>
-                            <TextInput
-                                value={state?.ticker}
-                                onChangeText={value => setState(prev => ({ ...prev, ticker: value }))}
-                                style={{ borderBottomWidth: 0.5, borderBottomColor: "#CCC", width: '50%', textAlign: 'center' }}
-                                placeholder="Ticker do ativo"
-                            />
-                        </View>
-
-                        <View style={styles.content}>
-                            <View style={styles.information}>
-                                <Text>Tipo: &nbsp;</Text>
-                                <Text>{getTypeFormatted(wallet?.type)}</Text>
-                            </View>
-
-                            <View style={styles.information}>
-                                <Text>Quantidade: &nbsp;</Text>
-                                <NumericInput
-                                    value={state?.quantity ? String(state.quantity) : wallet?.quantity ? String(wallet.quantity) : ''}
-                                    editable={!!(onSave)}
-                                    onChangeText={value => setState(prev => ({ ...prev, quantity: Number(value) }))}
-                                    onDecrease={newValue => setState(prev => ({ ...prev, quantity: Number(newValue) }))}
-                                    onIncrease={newValue => setState(prev => ({ ...prev, quantity: Number(newValue) }))}
+                            <View style={styles.header}>
+                                <TextInput
+                                    value={state?.ticker || wallet?.ticker || ''}
+                                    onChangeText={value => setState(prev => ({ ...prev, ticker: value }))}
+                                    style={{ borderBottomWidth: 0.5, borderBottomColor: "#CCC", width: '50%', textAlign: 'center' }}
+                                    placeholder="Ticker do ativo"
                                 />
                             </View>
-                        </View>
 
-                        <View style={styles.buttonsContainer}>
+                            <View style={styles.content}>
+                                <View style={styles.information}>
+                                    <Text>Tipo: &nbsp;</Text>
+                                    <Text>{getTypeFormatted(wallet?.type)}</Text>
+                                </View>
 
-                            <TouchableOpacity style={styles.button} onPress={onBackPress}>
-                                <Text>Voltar</Text>
-                            </TouchableOpacity>
+                                <View style={styles.information}>
+                                    <Text>Quantidade: &nbsp;</Text>
+                                    <NumericInput
+                                        value={state?.quantity ? String(state.quantity) : wallet?.quantity ? String(wallet.quantity) : ''}
+                                        editable={!!(onSave)}
+                                        onChangeText={value => setState(prev => ({ ...prev, quantity: Number(value) }))}
+                                        onDecrease={newValue => setState(prev => ({ ...prev, quantity: Number(newValue) }))}
+                                        onIncrease={newValue => setState(prev => ({ ...prev, quantity: Number(newValue) }))}
+                                    />
+                                </View>
+                            </View>
 
-                            {onSave && (
-                                <View style={styles.separator} />
-                            )}
+                            <View style={styles.buttonsContainer}>
 
-                            {onSave && (
-                                <TouchableOpacity
-                                    style={styles.button}
-                                    onPress={onSavePress}
-                                >
-                                    <Text>Salvar</Text>
+                                <TouchableOpacity style={styles.button} onPress={onBackPress}>
+                                    <Text>Voltar</Text>
                                 </TouchableOpacity>
-                            )}
+
+                                {onSave && (
+                                    <View style={styles.separator} />
+                                )}
+
+                                {onSave && (
+                                    <TouchableOpacity
+                                        style={styles.button}
+                                        onPress={onSavePress}
+                                    >
+                                        <Text>Salvar</Text>
+                                    </TouchableOpacity>
+                                )}
+
+                            </View>
 
                         </View>
-
                     </View>
-                </View>
-            </SafeAreaView>
+                </SafeAreaView>
+            </KeyboardAvoidingView>
         </Modal >
     );
 };
