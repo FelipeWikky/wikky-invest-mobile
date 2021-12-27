cadeea4198e3ceeac15aee6c1e98363f270d9f70
Felipe Lima <felipewikky@gmail.com>
2021-12-27 16:18:37 -0300

    Added calculate screen with components

--- src/screens/Calculate/index.tsx
+++ src/screens/Calculate/index.tsx
@@ -1,9 +1,38 @@
 import { View, Text } from "react-native";
+import { useCallback, useState } from "react";
+
+import WalletList from "../../components/WalletList";
+import useWallet from "../../hooks/useWallet";
+
+import styles from "./styles";
 
 const Calculate = () => {
+    const { wallets, } = useWallet();
+    const [total, setTotal] = useState(0);
+
+    const renderHeader = useCallback(() => (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>Seus ativos</Text>
+            <View style={styles.headerIcons}>
+                <Text>Dividend total: {total.toFixed(2)}</Text>
+            </View>
+        </View>
+    ), [total]);
+
+    const onCalculateCallback = useCallback((total: string | number, totalOld: string | number) => {
+        setTotal(prev => (prev - Number(totalOld)) + Number(total));
+    }, []);
+
     return (
-        <View>
-            <Text>Calculate</Text>
+        <View style={styles.container}>
+            {renderHeader()}
+            <View style={styles.content}>
+                <WalletList
+                    data={wallets}
+                    isCalculate={true}
+                    calculateCallback={onCalculateCallback}
+                />
+            </View>
         </View>
     );
 };
---
+++ src/screens/Calculate/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 5,
+        marginBottom: 5,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottomWidth: 0.2,
+        paddingVertical: 5,
+    },
+    headerTitle: {
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    headerIcons: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    content: {
+        height: '100%',
+        paddingTop: 16,
+    },
+    flatListItem: {
+        marginBottom: 16,
+    }
+});
